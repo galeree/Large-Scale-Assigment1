22723721351a8d613050c088b57912c61c7673ef
Patraporn Kuhavattana <galeguy@hotmail.com>
2016-02-06 15:18:41 +0700

    Modify map and reduce file to one file for easier usage.

--- Mapreduce_2/mapper_sort.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#-*-coding: utf-8 -*-
-
-import sys
-
-for line in sys.stdin:
-	line = line.strip()
-	word, count, filename = line.split('\t')
-	print '%s\t%s\t%s' % (filename, count, word)
